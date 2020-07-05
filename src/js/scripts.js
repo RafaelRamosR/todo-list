4f123b52eb02a658cd8f4d9ae4a0b8f626d1ec63
RRRR <rafael.ramosr@hotmail.com>
2020-07-05 08:33:49 -0500

    Correciones ESLint y Resonsive

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,38 +1,14 @@
-const form = document.getElementById("form"),
-  divTask = document.querySelector(".viewTask"),
-  fragment = document.createDocumentFragment()
+const form = document.getElementById('form'),
+  divTask = document.querySelector('.viewTask'),
+  fragment = document.createDocumentFragment();
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const et = e.target,
-    task = et.typeTask.value,
-    color = et.radioColor.value,
-    date = et.inputDate.value,
-    time = et.inputTime.value,
-    dateTime = date + " " + time,
-    // generate supremely secure ID xd
-    id = btoa(dateTime+Math.random())
-
-  if (!validateEmpty(task)) {
-    return validationMessage('Debe especificar el nombre de la tarea.')
-  }
-
-  if (!validateEmpty(color)) {
-    return validationMessage('Seleccione un color antes de enviar')
-  }
-
-  if (!validateDate(date, time)) {
-    return validationMessage('La fecha es invalida, ingrese una fecha superior a la fecha y hora actual.')
-  }
-
-  const taskObject = { 'id': id, 'task': task, 'color': color, 'date': dateTime }
-  form.reset()
-  // Save task
-  setStorage(taskObject.id, JSON.stringify(taskObject))
-  // Show task
-  getStorage(taskObject.id)
-  cleanMessage()
-})
+/*
+  Delete all tasks from localStorage
+*/
+document.getElementById('deleteAll').addEventListener('click', () => {
+  divTask.textContent = '';
+  localStorage.clear();
+});
 
 /*
   Generate the countdown of the task
@@ -41,15 +17,15 @@ const countDownDate = (dateTime) => {
   const second = 1000,
     minute = second * 60,
     hour = minute * 60,
-    day = hour * 24
+    day = hour * 24;
 
   // Set the date we're counting down to
-  const countDownDate = new Date(dateTime).getTime()
-  const taskDates = document.querySelectorAll(".task-date")
+  const countDownDatetime = new Date(dateTime).getTime(),
+    taskDates = document.querySelectorAll('.task-date');
   if (taskDates) {
     taskDates.forEach((taskDate) => {
-      const contentTask = document.createElement("ul")
-      /* 
+      const contentTask = document.createElement('ul');
+      /*
         Update the count down every 1 millisecond
         to show result as soon as page loads
       */
@@ -57,12 +33,12 @@ const countDownDate = (dateTime) => {
         // Get today's date and time
         const now = new Date().getTime(),
           // Find the distance between now and the count down date
-          distance = countDownDate - now,
+          distance = countDownDatetime - now,
           // Time calculations for days, hours, minutes and seconds
           days = Math.floor(distance / day),
           hours = Math.floor((distance % day) / hour),
           minutes = Math.floor((distance % hour) / minute),
-          seconds = Math.floor((distance % minute) / second)
+          seconds = Math.floor((distance % minute) / second);
         contentTask.innerHTML = `
           <ul>
             <li><span>${days}</span>Días</li>
@@ -70,81 +46,85 @@ const countDownDate = (dateTime) => {
             <li><span>${minutes}</span>Minutos</li>
             <li><span>${seconds}</span>Segundo</li>
           </ul>
-        `
+        `;
         // Show message when counter is at zero
         if (distance < 0) {
-          contentTask.innerHTML = `<p class="finish">TAREA FINALIZADA</p>`
+          contentTask.innerHTML = `
+            <p class="finish">TAREA FINALIZADA</p>
+          `;
         }
-      }, 1)
+      }, 1);
       // Show message when counter is at zero
-      if (taskDate.childNodes.length == 1) {
-        fragment.appendChild(contentTask)
-        taskDate.appendChild(contentTask)
+      if (taskDate.childNodes.length === 1) {
+        fragment.appendChild(contentTask);
+        taskDate.appendChild(contentTask);
       }
-
-    })
+    });
   }
-}
-
-/*
-  Create the task structure
-  And assign the corresponding attributes
-*/
-const insertData = (data) => {
-  // Task container and countdown
-  const contentTask = document.createElement("div")
-  contentTask.classList.add("task")
-  contentTask.classList.add(`${data.color}`)
-  contentTask.innerHTML = `
-    <div class="task-title">
-      <h1>${data.task}</h1>
-    </div>
-    <div class="task-date">
-    </div>
-  `
-  // Button that deletes the task
-  const deleteBtn = document.createElement("input")
-  deleteBtn.classList.add("btn-delete")
-  deleteBtn.setAttribute('type', 'button')
-  deleteBtn.setAttribute('data-key', data.id)
-  deleteBtn.setAttribute('value', 'X')
-  contentTask.appendChild(deleteBtn)
-  // Assign delete function to button
-  deleteBtn.addEventListener('click', (e) => {
-    deleteStorage(e.target.dataset.key)
-  })
-
-  fragment.appendChild(contentTask)
-  divTask.appendChild(fragment)
-  // Insert countdown when container is created
-  countDownDate(data.date)
-}
-
-/*
-  Save data to current localStorage
-*/
-const setStorage = (key, value) => {
-  localStorage.setItem(key, value)
-}
+};
 
 /*
   Access stored data
 */
 const getStorage = (key) => {
-  const data = JSON.parse(localStorage.getItem(key))
-  insertData(data)
-}
+  const data = JSON.parse(localStorage.getItem(key));
+  insertData(data);
+};
+
+/*
+  Save data to current localStorage
+*/
+const setStorage = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const et = e.target,
+    task = et.typeTask.value,
+    color = et.radioColor.value,
+    date = et.inputDate.value,
+    time = et.inputTime.value,
+    dateTime = `${date} ${time}`,
+    // generate supremely secure ID xd
+    id = btoa(dateTime + Math.random());
+
+  if (!validateEmpty(task)) {
+    return validationMessage('Debe especificar el nombre de la tarea.');
+  }
+
+  if (!validateEmpty(color)) {
+    return validationMessage('Seleccione un color antes de enviar');
+  }
+
+  if (!validateDate(date, time)) {
+    return validationMessage('La fecha es invalida, ingrese una fecha superior a la fecha y hora actual.');
+  }
+
+  const taskObject = {
+    id,
+    task,
+    color,
+    dateTime,
+  };
+  form.reset();
+  cleanMessage();
+  // Save task
+  setStorage(taskObject.id, JSON.stringify(taskObject));
+  // Show task
+  return getStorage(taskObject.id);
+});
 
 /*
   Iterate keys to show all tasks stored in localStorage
 */
 const getAllStorage = () => {
-  divTask.textContent = ''
-  for (let i = 0; i <= localStorage.length - 1; i++) {
-    let key = localStorage.key(i)
-    getStorage(key)
+  divTask.textContent = '';
+  for (let i = 0; i <= localStorage.length - 1; i += 1) {
+    const key = localStorage.key(i);
+    getStorage(key);
   }
-}
+};
 
 /*
   Delete an item from localStorage
@@ -152,16 +132,42 @@ const getAllStorage = () => {
   And show all tasks again to create update effect
 */
 const deleteStorage = (key) => {
-  localStorage.removeItem(key)
-  getAllStorage()
-}
+  localStorage.removeItem(key);
+  getAllStorage();
+};
 
 /*
-  Delete all tasks from localStorage
-*/ 
-document.getElementById("deleteAll").addEventListener('click', () => {
-  divTask.textContent = ''
-  localStorage.clear()
-})
+  Create the task structure
+  And assign the corresponding attributes
+*/
+const insertData = (data) => {
+  // Task container and countdown
+  const contentTask = document.createElement('div');
+  contentTask.classList.add('task');
+  contentTask.classList.add(`${data.color}`);
+  contentTask.innerHTML = `
+    <div class="task-title">
+      <h1>${data.task}</h1>
+    </div>
+    <div class="task-date">
+    </div>
+  `;
+  // Button that deletes the task
+  const deleteBtn = document.createElement('input');
+  deleteBtn.classList.add('btn-delete');
+  deleteBtn.setAttribute('type', 'button');
+  deleteBtn.setAttribute('data-key', data.id);
+  deleteBtn.setAttribute('value', 'X');
+  contentTask.appendChild(deleteBtn);
+  // Assign delete function to button
+  deleteBtn.addEventListener('click', (e) => {
+    deleteStorage(e.target.dataset.key);
+  });
 
-getAllStorage()
+  fragment.appendChild(contentTask);
+  divTask.appendChild(fragment);
+  // Insert countdown when container is created
+  countDownDate(data.dateTime);
+};
+
+getAllStorage();
--- src/js/validate.js
+++ src/js/validate.js
@@ -1,93 +1,95 @@
-/*
-  Validate that the input is not empty
-*/
-const validateEmpty = (input, type) => {
-  if(input.trim() === ""){
-    return false
+/* Validate that the input is not empty */
+const validateEmpty = (input) => {
+  if (input.trim() === '') {
+    return false;
   }
-  return true
-}
+  return true;
+};
 
-/*
-  Validate date and time
-*/
+/* Validate date and time */
 const validateDate = (date, time) => {
-  if(!validateEmpty(date) || !validateEmpty(time)){
-    return false
+  if (!validateEmpty(date) || !validateEmpty(time)) {
+    return false;
   }
 
-  // Get current date
-  const n =  new Date(),
-  year = n.getFullYear(),
   /*
+    Get current date
     The month is represented from 0 to 11, 0 = January and 11 = December
     1 is added to the obtained month and a 0 is prepended if it is less than 10
   */
-  mouth = n.getMonth() + 1,
-  month = mouth > 10 ? mouth : '0'+mouth,
-  day = n.getDate() > 10 ? n.getDate() : '0'+n.getDate()
-  
+  const n = new Date(),
+    year = n.getFullYear(),
+    month = n.getMonth() + 1,
+    day = n.getDate();
+
   // We obtain the year, month and day entered by the user
-  const dateYear = date.split('-')[0],
-  dateMonth = date.split('-')[1],
-  dateDay = date.split('-')[2]
+  const dateYear = parseInt(date.split('-')[0], 10),
+    dateMonth = parseInt(date.split('-')[1], 10),
+    dateDay = parseInt(date.split('-')[2], 10);
 
   /*
     If the selected year is less than the current year, bye
-    If the selected year is equal to the current one, but the month is less than the current month, bye again
+    If the selected year is equal to the current one,
+    but the month is less than the current month, bye again
   */
-  if(dateYear < year || dateYear == year && dateMonth < month){
-    return false
+  if (dateYear < year) {
+    return false;
+  }
+
+  if (dateYear === year && dateMonth < month) {
+    return false;
   }
 
   /*
-    If it reaches this point, it means that the year and month are greater than or equal to the current date
+    If it reaches this point, it means that the year
+    and month are greater than or equal to the current date
     If they are the same, the day cannot be allowed to be less than the current day
   */
-  if(dateYear == year && dateMonth == month && dateDay < day){
-    return false
+  if (dateYear === year && dateMonth === month && dateDay < day) {
+    return false;
   }
 
   // Get current time
   const hour = n.getHours(),
-  minute = n.getMinutes();
-
-  // Get the time selected by the user
-  const dateHour = time.split(':')[0],
-  dateMinute = time.split(':')[1];
+    minute = n.getMinutes(),
+    // Get the time selected by the user
+    dateHour = parseInt(time.split(':')[0], 10),
+    dateMinute = parseInt(time.split(':')[1], 10);
 
   //  The time only needs to be validated if the task is for today
-  if(dateYear == year && dateMonth == month && dateDay == day){
+  if (dateYear === year && dateMonth === month && dateDay === day) {
     /*
       If you select a time lower than the current time, bye
       If the hour is equal to the current one, the minutes cannot be less than the current minutes
     */
-    if(dateHour < hour || dateHour == hour && dateMinute < minute){
-      return false
+    if (dateHour < hour) {
+      return false;
+    }
+
+    if (dateHour === hour && dateMinute < minute) {
+      return false;
     }
   }
-  return true
-}
-
-/*
-  Show validation message on screen
-*/
-const validationMessage = (message) => {
-  cleanMessage()
-  // Create message
-  const contentMessage = document.createElement("div")
-  contentMessage.classList.add("error")
-  contentMessage.innerHTML = `
-    <p class="error-message">${message}</p>
-  `
-  //The message is inserted before the first child
-  fragment.prepend(contentMessage)
-  divTask.prepend(fragment)
-}
+  return true;
+};
 
 const cleanMessage = () => {
   // Possible previous messages are deleted
-  if(document.querySelector(".error")){
-    document.querySelector(".error").remove()
+  if (document.querySelector('.error')) {
+    document.querySelector('.error').remove();
   }
-}
+};
+
+/* Show validation message on screen */
+const validationMessage = (message) => {
+  cleanMessage();
+  // Create message
+  const contentMessage = document.createElement('div');
+  contentMessage.classList.add('error');
+  contentMessage.innerHTML = `
+    <p class="error-message">${message}</p>
+  `;
+  // The message is inserted before the first child
+  fragment.prepend(contentMessage);
+  divTask.prepend(fragment);
+};
